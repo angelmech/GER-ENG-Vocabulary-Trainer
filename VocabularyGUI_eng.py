a84fdb2d69dcc1a28c91f349aa87ccbc7daf9baa
angelmech <73247626+angelmech@users.noreply.github.com>
2021-02-11 20:21:44 +0100

    Update VocabularyGUI_eng.py

--- VocabularyGUI_eng.py
+++ VocabularyGUI_eng.py
@@ -4,24 +4,6 @@ import tkinter as tk
 import os
 import random
 
-#from cx_Freeze import setup, Executable
-#
-# sys.argv.append("build")  # replaces commandline arg 'build'
-#
-# # change the filename to your program file
-# filename = "VocabularyGUI_eng.py"
-#
-# base = None
-# if sys.platform == "win32":
-#     base = "Win32GUI"
-#
-# setup(
-#     name = "Vocabulary Trainer",
-#     version = "1.0",
-#     description = "cx_Freeze Tkinter script",
-#     executables = [Executable(filename, base=base)])
-
-
 window = Tk()
 
 window.title("Vocabulary Trainer")
